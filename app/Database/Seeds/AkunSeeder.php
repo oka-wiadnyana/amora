87fcd4893ddd4127446c4335c42f52f60e7e7e54
unknown <onsdee86@gmail.com>
2025-03-06 14:15:35 +0800

    copy migration

--- app/Database/Seeds/AkunSeeder.php
+++ app/Database/Seeds/AkunSeeder.php
@@ -8,10 +8,10 @@ class AkunSeeder extends Seeder
 {
     public function run()
     {
-        $password = password_hash('oka', PASSWORD_DEFAULT);
+        $password = password_hash('admin', PASSWORD_DEFAULT);
         $data = [
             'id_level' => '13',
-            'username'    => 'oka',
+            'username'    => 'admin',
             'password' => $password
         ];
 
